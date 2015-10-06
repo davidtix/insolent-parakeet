0eab59abfe92ed5def6aa704c1b7ff8dbf5db305
davidtix <david.steele@indyimaging.com>
2015-10-06 12:32:33 -0400

    going back a bit more
    wondering if my browser view is not working correctly until i make the
    commit

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 // script.js
 
     // create the module and name it scotchApp
-    var scotchApp = angular.module('scotchApp', ['ngRoute']);
+    var scotchApp = angular.module('scotchApp', [ngRoute]);
 
     //configure our routes
     scotchApp.config(function($routeProvider) {
-        $routeProvider
 
-              //route for the home page
-              .when('/', {
-                  templateUrl : 'pages/home.html',
-                  controller  : 'mainController'
-            })
+      //route for the home page
+      .when('/', {
+          templateUrl : 'pages/home.html',
+          controller  : 'mainController'
+      })
 
-            //route for the about page
-            .when('/about', {
-                templateUrl : 'pages/about.html',
-                controller  : 'aboutController'
-            })
+      //route for the about page
+      .when('/', {
+          templateUrl : 'pages/about.html',
+          controller  : 'aboutController'
+      })
 
-            //route for the about page
-            .when('/contact', {
-                templateUrl : 'pages/contact.html',
-                controller  : 'contactController'
-            });
+      //route for the about page
+      .when('/', {
+          templateUrl : 'pages/contact.html',
+          controller  : 'contactController'
+      });
     });
 
     // create the controller and inject Angular's $scope
     scotchApp.controller('mainController', function($scope) {
+
         // create a message to display in our view
         $scope.message = 'Everyone come and see how good I look!';
     });
